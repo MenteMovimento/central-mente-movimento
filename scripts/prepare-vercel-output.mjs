2106239c39a6e029a806ce47e701f0154edbdfb4
jf77ps <jf77ps@gmail.com>
2026-07-06 10:23:44 +0100

    Use activity timetable blocks

--- scripts/prepare-vercel-output.mjs
+++ scripts/prepare-vercel-output.mjs
@@ -37,7 +37,7 @@ const supabaseAnonKey =
   ''
 
 const jsString = (value) => JSON.stringify(String(value ?? ''))
-const assetVersion = '20260706-atividades-horario'
+const assetVersion = '20260706-atividades-blocos'
 
 const authPendingHead = `<script>
       (() => {
